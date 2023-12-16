a752ed3a93d7d8629fab352d1fc1c955b9442bb0
sillygabe <gabrielonline630@gmail.com>
2023-12-16 12:39:46 +0300

    Added base types :3

---
+++ scr/types.hpp
@@ -0,0 +1,20 @@
+#ifndef __TAUR_BASE_TYPES
+#define __TAUR_BASE_TYPES
+#endif
+
+namespace types
+{
+    typedef char int8_t;
+	typedef unsigned char uint8_t;
+	
+	typedef short int16_t;
+	typedef unsigned short uint16_t;
+
+    typedef int int32_t;
+    typedef unsigned int uint32_t;
+
+    typedef long long int64_t;
+    typedef unsigned long long size_t;
+
+    typedef char * string;
+} 
